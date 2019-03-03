afe9fd81020ffd4eefc41c2d3424057437042e90
lopezp9492 <43254797+lopezp9492@users.noreply.github.com>
2019-03-03 14:37:14 -0800

    Display and Send PID

--- UDPServer.java
+++ UDPServer.java
@@ -5,34 +5,63 @@
 import java.net.*;
 import java.io.*;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 public class UDPServer {
 
-	private static final int PORT = 6789;
+	private static final int PORT = 6790;
 	private static final int BUFFER_SIZE = 1000;
 
 	
 	public static void main(String[] args) {
+		//Get Process Name
+	    RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+        String jvmName = bean.getName();
+        System.out.println("Name = " + jvmName);
+        
+        // Extract the PID by splitting the string returned by the
+        // bean.getName() method.
+        long pid = Long.valueOf(jvmName.split("@")[0]);
+        System.out.println("PID  = " + pid);
+	    
+	    
 		DatagramSocket aSocket = null;
+		boolean running = true;
 		
 		try {
 			aSocket = new DatagramSocket(PORT);
 			byte[] buffer  = new byte[BUFFER_SIZE];
 			
-			while(true)
+			System.out.println("Server Started..." );
+
+			
+			
+			while(running)
 			{
 				// Receive
 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
 				aSocket.receive(request);
 				
+				//Print Clients Message;
+				System.out.println("Received: " + new String(request.getData() ) );
+	            
+				//Create Reply
+	            String mStr = "Hello from Server pid:" + pid;
+	            byte [] m =  mStr.getBytes(); // Message
+				
 				// Send
-				DatagramPacket reply = new DatagramPacket(request.getData(),
-						request.getLength(), request.getAddress(), request.getPort());
+				DatagramPacket reply = new DatagramPacket(m,
+						m.length, request.getAddress(), request.getPort());
 				aSocket.send(reply);
 			}
 			
 		}catch (SocketException e ) {System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e) {System.out.println("IO: " + e.getMessage());
-		}finally {if(aSocket != null)aSocket.close();}
+		}finally {
+			if(aSocket != null)aSocket.close();
+			System.out.println("Socket Closed." );
+		}
 
 	}
 
