b5d2661a3ee9f6f09ed3282fd2bbfcb2d1a91c76
lopezp9492 <43254797+lopezp9492@users.noreply.github.com>
2019-03-02 19:10:18 -0800

    Complete Book Example

--- UDPClient.java
+++ UDPClient.java
@@ -1,3 +1,7 @@
+// Distributed Systems 
+// Concepts and Design 5th Edition
+// Figure 4.3 UDP client sends a message to the server and gets a reply
+
 import java.net.*;
 import java.io.*;
 
@@ -7,7 +11,23 @@ public class UDPClient {
 		
 		DatagramSocket aSocket = null;
 		try {
+			
+			// SEND
 			aSocket = new DatagramSocket();
+			byte [] m args[0].getBytes(); //Message
+			InetAddress aHost = InetAddress.getByName(args[1]); // Server Address
+			int serverPort = 6789; // Server Port
+			
+			
+			DatagramPacket request = new DatagramPacket (m, m.length, aHost, serverPort);
+			aSocket.send(request);
+			
+			//RECEIVE
+			byte[] buffer = new byte[1000];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(reply);
+			
+			System.out.println("Reply: " + new String(reply.getData()));
 			
 		}catch (SocketException e ) {System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e) {System.out.println("IO: " + e.getMessage());
